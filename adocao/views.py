06a45705b1ef787b6a35cb961211658cff0f5110
Andre Carlos <andresinho200498@gmail.com>
2022-06-06 23:21:06 -0300

    Configurar envio de email e enviar email apos adoção

--- adocao/views.py
+++ adocao/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 
+from .email_service import enviar_email_confirmacao
 from .serializers import AdocaoSerializer
 from .models import Adocao
 
@@ -15,7 +16,8 @@ class AdocaoList(APIView):
     def post(self, req, format=None):
         deserializer = AdocaoSerializer(data=req.data)
         if deserializer.is_valid():
-            deserializer.save()
+            adocao = deserializer.save()
+            enviar_email_confirmacao(adocao)
             return Response(deserializer.data, status=HTTP_201_CREATED)
         else:
             return Response(
