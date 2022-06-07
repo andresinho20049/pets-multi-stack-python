ceb43dc74260c4fb54d673b3e6faeefa03291e56
Andre Carlos <andresinho200498@gmail.com>
2022-06-06 23:20:43 -0300

    Post Request Pet, cadastro PET

--- pet/views.py
+++ pet/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.status import HTTP_200_OK
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 
 from .models import Pet
 from .serializers import PetSerializers
@@ -11,3 +11,16 @@ class PetList(APIView):
         pets = Pet.objects.all()
         serializer = PetSerializers(pets, many=True)
         return Response(serializer.data, status=HTTP_200_OK)
+
+    def post(self, request, fromat=None):
+        serializer = PetSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=HTTP_201_CREATED)
+        return Response(
+            {
+                "message": "Houveram erros de validação",
+                "errors": serializer.errors,
+            },
+            status=HTTP_400_BAD_REQUEST,
+        )
